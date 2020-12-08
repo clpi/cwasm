d79385234ef70c36409341ce7fe8d4d1f29e7c40
Chris P <chrisp1877@gmail.com>
2020-12-07 22:32:27 -0800

    adding wasm code with web-sys, spec. helper functions to create html elements

--- src/App.svelte
+++ src/App.svelte
@@ -1,10 +1,24 @@
 <script>
 	export let name;
+	let contactName = "";
+
+  const contact = () => {
+    if (contactName != "") {
+        open("mailto://chris@pecu.io");
+    }
+  }
 </script>
 
 <main>
-	<h1>Welcome to clp.is</h1>
+  <h1>{ name }</h1>
+  <h2>welcome to clp.is</h2>
   <p>I'm Chris Pecunies. This website is under construction -- visit <a href="http://chris.pecunies.com">my other website</a> instead</p>
+  <form name="contact">
+    <span class="contactPrompt">Your name? </span>
+    <input name="contactName" bind:value={contactName}/>
+    <button on:click={contact}>contact me</button>
+    </form>
+  <hr/>
 </main>
 
 <style>
@@ -14,6 +28,12 @@
 		max-width: 240px;
 		margin: 0 auto;
 	}
+  .contactPrompt {
+    color: #ff6e44;
+    text-transform: lowercase;
+    font-weight: 400;
+    padding-right: 20px;
+  }
 
 	h1 {
 		color: #ff3e00;
@@ -21,6 +41,12 @@
 		font-size: 4em;
 		font-weight: 100;
 	}
+  h2 {
+    color: #ff6e44;
+    text-transform: lowercase;
+    font-size: 2.2em;
+    font-weight: 300;
+  }
 
 	@media (min-width: 640px) {
 		main {
